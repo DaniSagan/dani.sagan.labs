bf8be6f950044d1e978a2780c217e081df208532
Daniel Fernández Villanueva <danielfervilus@gmail.com>
2024-09-21 23:47:17 +0200

    Add some messages when computing pi.

--- src/app/tools/pi-decimal/pi-decimal.component.ts
+++ src/app/tools/pi-decimal/pi-decimal.component.ts
@@ -15,16 +15,26 @@ export class PiDecimalComponent {
   piDecimals: string = '';
   decimalPartLength = 100;
   calculating = false;
+  calculatingArctanMessage: string = '';
+  calculatingTermMessage: string = '';
 
   // Función para calcular Pi con n decimales usando Big.js
   async calculatePi(n: number): Promise<string> {
     Big.DP = n + 10; // Añadimos extra precisión para evitar errores de redondeo
 
+    let startTime = performance.now();
     // Usamos la fórmula de Machin para calcular Pi
+    this.calculatingArctanMessage = 'Calculando arctan(1/5)';
     const arctan1_5 = await this.arctan(new Big(1).div(5), n + 10);
+    this.calculatingArctanMessage = 'Calculando arctan(1/239)';
     const arctan1_239 = await this.arctan(new Big(1).div(239), n + 10);
 
     const pi = new Big(4).times(new Big(4).times(arctan1_5).minus(arctan1_239));
+
+    let endTime = performance.now();
+
+    this.calculatingArctanMessage = '';
+    this.calculatingTermMessage = `Tiempo total: ${(endTime - startTime)/1000} s.`;
 
     // Convertimos el resultado a string y redondeamos a n decimales
     return pi.toFixed(n, Big.roundDown);
@@ -36,8 +46,9 @@ export class PiDecimalComponent {
     let xTerm = x;
     let x2 = x.times(x); // x^2
     let i = 1;
+    let precisionValue = new Big(10).pow(-precision);
 
-    while (xTerm.abs().gt(new Big(10).pow(-precision))) {
+    while (xTerm.abs().gt(precisionValue)) {
       if(((i-1)/2) % 2 == 0) {
         result = result.plus(xTerm.div(i));
       } else {
@@ -45,6 +56,7 @@ export class PiDecimalComponent {
       }
       i = i += 2;
       xTerm = xTerm.times(x2);
+      this.calculatingTermMessage = `Término actual: ${xTerm.toExponential(6)}, meta de precisión: ${precisionValue.toString()}`;
       await this.sleep(0);
     }
 
