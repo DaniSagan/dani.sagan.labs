5931b1d49f63499475084e939892f73487a5628e
Daniel Fernández Villanueva <danielfervilus@gmail.com>
2024-09-24 12:25:38 +0200

    Move controls of implicit curve plotter to component.

--- src/app/tools/implicit-curve-graph-tool/implicit-curve-graph-tool.component.ts
+++ src/app/tools/implicit-curve-graph-tool/implicit-curve-graph-tool.component.ts
@@ -1,14 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, Input, ViewChild } from '@angular/core';
 import { WidgetsModule } from 'src/app/widgets/widgets.module';
 import { ImplicitCurveGraphComponent } from "../../widgets/implicit-curve-graph/implicit-curve-graph.component";
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-implicit-curve-graph-tool',
   standalone: true,
-  imports: [WidgetsModule, ImplicitCurveGraphComponent],
+  imports: [WidgetsModule, ImplicitCurveGraphComponent, FormsModule],
   templateUrl: './implicit-curve-graph-tool.component.html',
   styleUrl: './implicit-curve-graph-tool.component.css'
 })
 export class ImplicitCurveGraphToolComponent {
+  @ViewChild('curveGraph', {static: true}) curveGraph!: ImplicitCurveGraphComponent;
+  formula: string = 'x*x + y*y - 1';
+  xMin: number = -10;
+  xMax: number = 10;
+  yMin: number = -10;
+  yMax: number = 10;
 
+  onRedraw() {
+    if(this.validateFormula(this.formula)) {
+      const replacedFormula = new Function('x', 'y', `return ${this.formula}`);
+      this.curveGraph.setBounds(this.xMin, this.xMax, this.yMin, this.yMax);
+      this.curveGraph.drawGraph(replacedFormula);
+    }
+  }
+
+  private validateFormula(formula: string) {
+    if(formula.indexOf('x') < 0) return false;
+    if(formula.indexOf('y') < 0) return false;
+    return true;
+  }
 }
--- src/app/widgets/implicit-curve-graph/implicit-curve-graph.component.ts
+++ src/app/widgets/implicit-curve-graph/implicit-curve-graph.component.ts
@@ -11,7 +11,16 @@ import { FormsModule } from '@angular/forms';
 export class ImplicitCurveGraphComponent {
   @ViewChild('graphCanvas', { static: true }) canvas!: ElementRef<HTMLCanvasElement>;
 
-  @Input() formula: string = 'x*x + y*y - 1'; // Fórmula inicial: círculo unitario
+  _formula: string = 'x*x + y*y - 1'; // Fórmula inicial: círculo unitario
+
+  _formulaFormula: Function;
+  @Input()
+  get formula(): Function { return this._formulaFormula; }
+  set formula(fn: Function) {
+    this._formulaFormula = fn;
+    this.drawGraph(this._formulaFormula);
+  }
+
   @Input() xMin: number = -10;
   @Input() xMax: number = 10;
   @Input() yMin: number = -10;
@@ -20,22 +29,29 @@ export class ImplicitCurveGraphComponent {
 
   context!: CanvasRenderingContext2D;
 
+  constructor() {
+    this._formulaFormula = new Function('x', 'y', 'x - y');
+  }
+
   ngOnInit() {
     this.context = this.canvas.nativeElement.getContext('2d')!;
-    if(this.validateFormula(this.formula)) {
-      this.drawGraph();
+    if(this.validateFormula(this._formula)) {
+      const replacedFormula = new Function('x', 'y', `return ${this._formula}`);
+      this.drawGraph(replacedFormula);
     }
   }
 
   // Dibujar la gráfica implícita f(x, y) = 0
-  drawGraph() {
-    console.log('formula', this.formula);
+  drawGraph(fn: Function) {
+    console.log('formula', this._formula);
     const canvas = this.canvas.nativeElement;
     const ctx = this.context;
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 
     const width = canvas.width;
     const height = canvas.height;
+    ctx.fillStyle = 'white';
+    ctx.fillRect(0, 0, width, height);
 
     // Escala para convertir valores matemáticos a píxeles
     const xScale = width / (this.xMax - this.xMin);
@@ -53,28 +69,29 @@ export class ImplicitCurveGraphComponent {
     // Dibujar la curva implícita
     ctx.beginPath();
     ctx.strokeStyle = 'blue';
+    ctx.fillStyle = 'black';
 
-    const replacedFormula = new Function('x', 'y', `return ${this.formula}`);
+    //const replacedFormula = new Function('x', 'y', `return ${this.formula}`);
     for (let xPixel = 0; xPixel < width; xPixel++) {
       for (let yPixel = 0; yPixel < height; yPixel++) {
-        const xValue0 = this.xMin + (xPixel / xScale);
+        const xValue0 = this.xMin + ((xPixel) / xScale);
         const xValue1 = this.xMin + ((xPixel + 1) / xScale);
-        const yValue0 = this.yMax - (yPixel / yScale); // El canvas tiene el eje Y invertido
-        const yValue1 = this.yMax - ((yPixel+1) / yScale);
+        const yValue0 = this.yMax - ((yPixel) / yScale); // El canvas tiene el eje Y invertido
+        const yValue1 = this.yMax - ((yPixel + 1) / yScale);
 
         let fValue0: number;
         let fValueX1: number;
         let fValueY1: number;
         try {
-          fValue0 = replacedFormula(xValue0, yValue0);
-          fValueX1 = replacedFormula(xValue1, yValue0);
-          fValueY1 = replacedFormula(xValue0, yValue1);
+          fValue0 = fn(xValue0, yValue0);
+          fValueX1 = fn(xValue1, yValue0);
+          fValueY1 = fn(xValue0, yValue1);
         } catch (e) {
           console.error('Error evaluando la fórmula:', e);
           throw e;
         }
 
-        if (fValue0 * fValueX1 < 0 || fValue0 * fValueY1 < 0) {
+        if (fValue0 * fValueX1 <= 0 || fValue0 * fValueY1 <= 0) {
           ctx.fillRect(xPixel, yPixel, 1, 1);
         }
       }
@@ -85,7 +102,17 @@ export class ImplicitCurveGraphComponent {
 
   // Método para redibujar la gráfica cuando cambien los parámetros
   onRedraw() {
-    this.drawGraph();
+    if(this.validateFormula(this._formula)) {
+      const replacedFormula = new Function('x', 'y', `return ${this._formula}`);
+      this.drawGraph(replacedFormula);
+    }
+  }
+
+  setBounds(xMin: number, xMax: number, yMin: number, yMax: number): void {
+    this.xMin = xMin;
+    this.xMax = xMax;
+    this.yMin = yMin;
+    this.yMax = yMax;
   }
 
   private validateFormula(formula: string) {
